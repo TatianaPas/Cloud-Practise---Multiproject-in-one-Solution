d8a55117750221c2e7020746b584855d092a0fed
Tatiana <tatjana.pasechnik@gmail.com>
2022-09-06 13:50:23 +1200

    initial commit

---
+++ EmployeeDetails/EmployeeDetails/Controllers/EmpployeeClientController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeDetails.Controllers
+{
+    public class EmpployeeClientController : Controller
+    {
+        //https://localhost:7221/EmpployeeClient/Login
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public string Login(string userid, string password)
+        {
+            string message = "Success";
+            if(userid != "admin")
+                {
+                message = "Failure";
+            }
+                if(password!="admin")
+            {
+                message = "Failure";
+            }
+            
+            return message;
+        }
+    }
+}
